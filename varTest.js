d1bd0af09a2d3aab3e16fa10f58a6826d2bb65a6
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-26 02:35:35 +0900

    js 변수 생성해보기

---
+++ varTest.js
@@ -0,0 +1,15 @@
+/* 지시사항 1번 내용을 코드로 작성해보세요. */
+var fruit = "apple";
+
+/* 지시사항 2번 내용을 코드로 작성해보세요. */
+var box = "banana";
+
+/* 지시사항 3번 내용을 코드로 작성해보세요. */
+document.write(fruit);
+document.write(box);
+
+/* 지시사항 4번 내용을 코드로 작성해보세요. */
+box = "tomato";
+
+/* 지시사항 5번 내용을 코드로 작성해보세요. */
+document.write(box);
