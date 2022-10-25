c7cf1fa26a397baf53006303530684e77630a224
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-26 08:22:56 +0900

    js array 메소드 실습

---
+++ arrayMethod.js
@@ -0,0 +1,23 @@
+/* 지시사항 1번 내용을 코드로 작성해보세요. */
+var fruit = ["Apple", "Banana", "Tomato"];
+
+/* 지시사항 2번 내용을 코드로 작성해보세요. */
+document.write(fruit.length);
+
+/* 지시사항 3번 내용을 코드로 작성해보세요. */
+fruit.push("A");
+
+/* 지시사항 4번 내용을 코드로 작성해보세요. */
+fruit.unshift("B");
+
+/* 지시사항 5번 내용을 코드로 작성해보세요. */
+document.write(fruit);
+
+/* 지시사항 6번 내용을 코드로 작성해보세요. */
+fruit.pop();
+
+/* 지시사항 7번 내용을 코드로 작성해보세요. */
+fruit.shift();
+
+/* 지시사항 8번 내용을 코드로 작성해보세요. */
+document.write(fruit);
