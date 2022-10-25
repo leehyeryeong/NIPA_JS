e861271f4bb5c5aafc197c90ccbc9fcae196019b
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-26 08:20:32 +0900

    js string 메소드 실습

---
+++ stringMethod.js
@@ -0,0 +1,11 @@
+/* 지시사항 1번 내용을 코드로 작성해보세요. */
+var str1 = "Hello World";
+
+/* 지시사항 2번 내용을 코드로 작성해보세요. */
+document.write(str1.length);
+
+/* 지시사항 3번 내용을 코드로 작성해보세요. */
+document.write(str1.charAt(0));
+
+/* 지시사항 4번 내용을 코드로 작성해보세요. */
+document.write(str1.split(" "));
