88c6bbebfe08e12dae215ca25cc9af4b2e08a516
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-26 07:56:25 +0900

    js 문자열 실습

---
+++ stringTest.js
@@ -0,0 +1,13 @@
+/* 지시사항 1번 내용을 코드로 작성해보세요. */
+var str1 = "Hello World";
+
+/* 지시사항 2번 내용을 코드로 작성해보세요. */
+var str2 = "Nice to meet you";
+
+/* 지시사항 3번 내용을 코드로 작성해보세요. */
+var str3 = "She's a girl";
+
+/* 지시사항 4번 내용을 코드로 작성해보세요. */
+document.write(str1);
+document.write(str2);
+document.write(str3);
