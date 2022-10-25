daeda6e9591fe14437549c89ab3904f1ae633901
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-26 08:01:02 +0900

    js 함수 실습

---
+++ functionTest.js
@@ -0,0 +1,15 @@
+/* 지시사항 1번 내용을 코드로 작성해보세요. */
+var sum = function(num1, num2) {
+    return num1 + num2;
+}
+
+/* 지시사항 2번 내용을 코드로 작성해보세요. */
+document.write(sum(10, 20));
+
+/* 지시사항 3번 내용을 코드로 작성해보세요. */
+function mul(num3, num4) {
+    return num3 * num4;
+}
+
+/* 지시사항 4번 내용을 코드로 작성해보세요. */
+document.write(mul(3,4));
