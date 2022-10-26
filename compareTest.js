48d96df613b83bde512827ee8da892a540a5e8b4
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-26 10:08:50 +0900

    js 비교 연산자 실습

---
+++ compareTest.js
@@ -0,0 +1,16 @@
+/* 지시사항 1번 내용을 코드로 작성해보세요. */
+document.writeln(10 == 10);
+document.writeln(10 === 10);
+
+/* 지시사항 2번 내용을 코드로 작성해보세요. */
+document.writeln(10 == "10");
+document.writeln(10 === "10");
+
+/* 지시사항 3번 내용을 코드로 작성해보세요. */
+document.writeln(10 !== 20);
+
+/* 지시사항 4번 내용을 코드로 작성해보세요. */
+document.writeln(10 > 20);
+document.writeln(10 >= 20);
+document.writeln(10 < 20);
+document.writeln(10 <= 20);
