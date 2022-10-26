55932eada0732373521c431b22bb153c355296a7
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-26 10:48:55 +0900

    js 반복문 실습

---
+++ elseifTest.js
@@ -0,0 +1,29 @@
+/* 지시사항 1번 내용을 코드로 작성해보세요. */
+var a = 20;
+var b = 40;
+var c = 60;
+
+/* 지시사항 2번 내용을 코드로 작성해보세요. */
+if (a > b) {
+    document.writeln("a > b");
+}
+
+/* 지시사항 3번 내용을 코드로 작성해보세요. */
+else if (b > c) {
+    document.writeln("b > c");
+}
+
+/* 지시사항 4번 내용을 코드로 작성해보세요. */
+else if (a < c) {
+    document.writeln("a < c");
+}
+
+/* 지시사항 5번 내용을 코드로 작성해보세요. */
+else if (b < c) {
+    document.writeln("b < c");
+}
+
+/* 지시사항 6번 내용을 코드로 작성해보세요. */
+else {
+    document.writeln("모든 조건을 만족하지 않는다.");
+}
