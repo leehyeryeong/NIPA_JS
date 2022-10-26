e3f4cf62c626cafffe5c90f6fb7368ee9ccc7aa8
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-26 10:06:35 +0900

    js 증감 연산자 실습

---
+++ increaseTest.js
@@ -0,0 +1,16 @@
+/* 지시사항 1번 내용을 코드로 작성해보세요. */
+var num = 20;
+
+/* 지시사항 2번 내용을 코드로 작성해보세요. */
+document.write(++num);
+
+/* 지시사항 3번 내용을 코드로 작성해보세요. */
+document.write(--num);
+
+/* 지시사항 4번 내용을 코드로 작성해보세요. */
+document.write(num++);
+document.write(num);
+
+/* 지시사항 5번 내용을 코드로 작성해보세요. */
+document.write(num--);
+document.write(num);
