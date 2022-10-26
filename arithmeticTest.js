ec254d1683ccb677c195d9cb23424510849b8cc8
leehyeryeong <w2108@e-mirim.hs.kr>
2022-10-26 10:05:47 +0900

    js 산술 연산자 실습

---
+++ arithmeticTest.js
@@ -0,0 +1,16 @@
+/* 지시사항 1번 내용을 코드로 작성해보세요. */
+document.write(20 + 10);
+document.write(20 - 10);
+document.write(20 * 10);
+document.write(20 / 10);
+document.write(20 % 10);
+
+/* 지시사항 2번 내용을 코드로 작성해보세요. */
+document.write("20" + "10");
+document.write("20" - "10");
+document.write("20" * "10");
+document.write("20" / "10");
+document.write("20" % "10");
+
+/* 지시사항 3번 내용을 코드로 작성해보세요. */
+document.write("Elice " + "Lee");
